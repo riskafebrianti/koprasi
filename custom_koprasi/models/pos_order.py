0166e79283fbf07d5209ec7293a9c59a35e2eed4
unknown <riskaafebri@gmail.com>
2025-01-24 17:36:28 +0800

    typo

--- custom_koprasi/models/pos_order.py
+++ custom_koprasi/models/pos_order.py
@@ -4,7 +4,7 @@ from odoo.exceptions import UserError
 class Pos_orderan(models.Model):
     _inherit = 'pos.order'
 
-    method_pay = fields.Char(string='Product name', compute='method_payy',)
+    method_pay = fields.Char(string='Payment Method', compute='method_payy',)
     # payment = fields.One2many('pos.payment', 'order_id', string='Riwayat Jual')
     # payment_ids = fields.One2many('pos.payment', 'pos_order_id', string='Payments', readonly=True)
     # payment_method = fields.Many2one('pos.payment', string='Kasir')
