a8314c40e194a0211f2fe944848413b95105c718
riskafebrianti <riskaafebri@gmail.com>
2025-04-18 22:46:29 +0800

    detail order

--- custom_koprasi/models/pos_order_line.py
+++ custom_koprasi/models/pos_order_line.py
@@ -5,7 +5,22 @@ class linePOS(models.Model):
     _inherit = 'pos.order.line'
 
     toko = fields.Char(string='Toko', related='order_id.config_id.name',store=True)
-    payment_method = fields.Char(string='Paymen Method', related='order_id.payment_ids.payment_method_id.name',store=True)
+    payment_method = fields.Char(string='Payment Method', related='order_id.payment_ids.payment_method_id.name',store=True)
     partner_ids = fields.Char(string='Customer',related='order_id.partner_id.name',)
+    date_orders = fields.Date(string='Date Order', compute='compute_relate',)
+    refund = fields.Char(string='Keterangan',compute='compute_relate')
 
+    # @api.depends('order_id.state')
+    def compute_relate(self):
+        for rec in self:
+            rec.date_orders = rec.order_id.date_order
+            if rec.order_id.name.split(" ")[-1] == 'REFUND':
+                rec.refund = 'REFUND'
+            else:
+                rec.refund = ''
+
+
+    def action_refresh_compute(self):
+    # for record in self:
+        self.compute_relate()
     
